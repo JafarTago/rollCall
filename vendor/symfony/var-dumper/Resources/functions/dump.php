36f365c983cb12bea7b3a05b4d2c40caf5ded22b
jafar <jafartago@gmail.com>
2020-01-25 22:03:51 +0800

    push vender

---
+++ vendor/symfony/var-dumper/Resources/functions/dump.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\Component\VarDumper\VarDumper;
+
+if (!function_exists('dump')) {
+    /**
+     * @author Nicolas Grekas <p@tchwork.com>
+     */
+    function dump($var, ...$moreVars)
+    {
+        VarDumper::dump($var);
+
+        foreach ($moreVars as $v) {
+            VarDumper::dump($v);
+        }
+
+        if (1 < func_num_args()) {
+            return func_get_args();
+        }
+
+        return $var;
+    }
+}
+
+if (!function_exists('dd')) {
+    function dd(...$vars)
+    {
+        list($callee) = debug_backtrace();
+
+        echo '<legend style="font-family: Microsoft JhengHei; color:#E87E04; padding:5px;font-size:18px;">'.$callee['file'].':'.$callee['line'].'</legend><pre><code>';
+
+        foreach ($vars as $v) {
+            VarDumper::dump($v);
+        }
+
+        die(1);
+    }
+}
